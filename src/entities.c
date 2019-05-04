f04b8e12545752749014a00fd696755f684b259a
woohook <woohook@localhost.net>
2019-05-05 00:16:49 +0200

    Added entity value and a function for finding entities by name

--- src/entities.c
+++ src/entities.c
@@ -35,10 +35,34 @@ struct _entity* entity_create(struct _entity* parent, char* name, char* entity_t
   pEntity->entity_type = entity_type;
   pEntity->name = fullname;
   pEntity->parent = parent;
+  pEntity->children = 0;
+  pEntity->value = 0;
 
   list_add_value(g_entities, pEntity);
 
   return pEntity;
+}
+
+struct _entity* entity_get_by_name(char* name)
+{
+  struct _entity* pEntityResult = 0;
+
+  struct _list_item* entity_node = list_get_first(g_entities);
+
+  while(entity_node != 0)
+  {
+    struct _entity* pEntity = list_item_get_value(entity_node);
+
+    if(strcmp(name, pEntity->name) == 0)
+    {
+      pEntityResult = pEntity;
+      break;
+    }
+
+    entity_node = list_item_get_next(entity_node);
+  }
+
+  return pEntityResult;
 }
 
 void entity_list_all()
--- src/entities.h
+++ src/entities.h
@@ -4,10 +4,13 @@ struct _entity
 {
   char* entity_type;
   char* name;
+  void* value;
   struct _entity* parent;
   struct _list* children;
 };
 
 struct _entity* entity_create(struct _entity* parent, char* name, char* entity_type, int size);
+
+struct _entity* entity_get_by_name(char* name);
 
 void entity_list_all();
